2abb65e00c8be2c1518d315c63d863e0142d2b79
Gabriel Panza <gabrieljpanza@gmail.com>
2022-11-18 10:27:16 -0300

    update interface

--- Exercises-List/Interface/LearningProcess/HelloWorld.java
+++ Exercises-List/Interface/LearningProcess/HelloWorld.java
@@ -1,26 +1,31 @@
 import javax.swing.*;
-//import javax.swing.event.*;
+// import javax.swing.event.*;
+// import java.awt.*;
+// import java.awt.event.*;
 
 public class HelloWorld
 {
     public static void main(String[] args) 
     {
-        JFrame frame;
-        frame = new JFrame("Hello World");
+        JFrame frame = new JFrame("Hello World");
         frame.setSize(1280,720);
         frame.setVisible(true);    
         
-        JPanel painel;
-        painel = new JPanel ();
-        frame.getContentPane().add (painel);
+        JPanel painel = new JPanel ();
+        painel.setSize(1280,720);
         painel.setVisible(true);
-
-        ImageIcon icone = new ImageIcon("images/barra.jpg");
+        
+        ImageIcon icone = new ImageIcon("images/barra.png");
         JButton botao = new JButton ("Hello World", icone);
+        botao.setBounds(frame.getWidth()/2 - 150, frame.getHeight()/2 - 50, 200, 45);
         botao.setVerticalTextPosition (AbstractButton.CENTER);
         botao.setHorizontalTextPosition (AbstractButton.LEFT);
         botao.setToolTipText ("Hello World means 'hi to the world'");
         botao.setEnabled (true);
+        
+        painel.add(botao);
+        frame.getContentPane().add(painel);
+        
         boolean visivel = botao.isVisible();
         if (visivel)
             System.out.println("Botao Visivel");
