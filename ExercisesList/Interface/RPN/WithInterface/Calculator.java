dc51d54d5c1f8182df78fd79d441fb8198a0594b
Gabriel Panza <110632615+Gabriel-Panza@users.noreply.github.com>
2022-11-28 19:51:15 -0300

    Update RPN

--- ExercisesList/Interface/RPN/WithInterface/Calculator.java
+++ ExercisesList/Interface/RPN/WithInterface/Calculator.java
@@ -1,11 +1,11 @@
-package Interface.RPN;
+package Interface.RPN.WithInterface;
 
 import javax.swing.*;
 import javax.swing.event.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class RpnWithInterface 
+public class Calculator 
 {
     public static void main(String[] args) 
     {
---
+++ ExercisesList/Interface/RPN/WithoutInterface/Calculator.java
@@ -0,0 +1,28 @@
+package Interface.RPN.WithoutInterface;
+
+import java.util.Scanner;
+
+public class Calculator 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Calculadora NPR:");
+        System.out.println("Digite '.exit' a qualquer momento para sair");
+        String inp = "";
+        Scanner teclado = new Scanner(System.in);
+
+        while(inp != ".exit")
+        {
+            System.out.println("Digite uma expressao NPR:");
+            System.out.print("-> ");
+            inp = teclado.nextLine();
+            try{
+                System.out.println(RPN.evaluate(inp));
+            } catch(Exception e){
+                System.out.println("Invalid Expression, Try again or type '.exit' to quit");
+            }
+        }
+
+        teclado.close();
+    }
+}
