01010c5d5753548821f566da3b32b7e259cdcb77
Gabriel Panza <110632615+Gabriel-Panza@users.noreply.github.com>
2022-12-04 00:01:28 -0300

    Update RPN.java

--- ExercisesList/Interface/RPN/WithInterface/RPN.java
+++ ExercisesList/Interface/RPN/WithInterface/RPN.java
@@ -14,11 +14,11 @@ public class RPN
 
         do
         {
-            int space = expr.substring(start).indexOf(' ');       // Defino o que eh um espaço
-            int end = (space == -1 ? expr.length() : start + space); // Defino o final da minha expressao com base no tamanho no espaco de cima
+            int space = expr.substring(start).indexOf(' ');       // Defino onde esta o espaco
+            int end = (space == -1 ? expr.length() : start + space); // Defino ate onde a minha substring vai (determino o final do numero).
 
-            String current = expr.substring(start,end);        // numero ou operador atual
-            if("+-*/".indexOf(current.charAt(0)) != -1) // Se atual eh um operador realiza as operaçoes necessarias
+            String current = expr.substring(start,end);             // Defino ate onde o numero ou operador atual vai (paro ao encontarr um espaço)
+            if("+-*/".indexOf(current.charAt(0)) != -1)      // Se atual eh um operador realiza as operaçoes necessarias
             {
                 //Removo dois da pilha e aplico a operaçao
                 Double a = stack.pop();
@@ -27,16 +27,14 @@ public class RPN
             }
             else // Se atual eh um operando adiciono na pilha
             {
-                if (current == "i")
-                    current = Double.toString(Math.sqrt(-1));
                 stack.push(Double.parseDouble(current));
             }
-            start = end + 1;            // Recomeço a partir de um novo inicio
-        }while(start < expr.length()); // Faço isso ate a expressao terminar
+            start = end + 1;            // Recomeço a partir do proximo numero ou operador
+        }while(start < expr.length());  // Faço isso ate a expressao terminar
 
-        double result = stack.pop(); // Retiro o unico numero que sobrou na pilha e armazeno na avriavel resultado
+        double result = stack.pop();    // Retiro o unico numero que sobrou na pilha e armazeno na variavel resultado
 
-        while(!stack.isEmpty())    // Se a pilha nao estiver vazia retorno o maior valor
+        while(!stack.isEmpty())         // Se a pilha nao estiver vazia retorno o maior valor
         {
             double current = stack.pop();
             result = current > result ? current : result;
